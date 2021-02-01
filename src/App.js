0c499866c570585ff2a2fd997ae29259c8f177d9
unknown <yestsering@gmail.com>
2021-02-01 11:15:14 +0100

    slider commit

--- src/App.js
+++ src/App.js
@@ -10,14 +10,72 @@ const heartMax = 180;
 const stepsMin = 0;
 const stepsMax = 50000;
 export class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      water : 1.5,
+      heart : 120,
+      temperature : -10,
+      steps : 3000,
+    }
+    
+  }
+  onHeartChange(event) {
+    
+    this.setState({
+      heart: event.target.value
+    });
+  }
+  onStepsChange(event) {
+    this.setState({
+      steps: event.target.value
+    })
+  }
+  onTemperatureChange(event){
+    if(this.state.temperature > 20){
+      this.state.water = this.state.water + 0.02;
+    }
+    this.setState({
+      
+      temperature: event.target.value
+    })
+  }
+
+  calculateWater() {
+    this.setState({
+      water: this.state.water
+    })
+  }
   render() {
     return (
       <div className= "container-fluid">
         <div className="row">
-          <Box icon ={"local_drink"} color= {"#3A85FF"} value = {1.5} unit = {"L"}/>
-          <Box icon ={"directions_walk"} color= {"black"} value = {3000} unit = {"Steps"}/>
-          <Box icon ={"favorite"} color= {"red"} value = {120} unit = {"bpm"}/>
-          <Box icon ={"wb_sunny"} color= {"yellow"} value = {-10} unit = {"°C"}/>
+          <Box icon ={"local_drink"} 
+               color= {"#3A85FF"} 
+               value = {this.state.water} 
+               unit = {"L"}/>
+          <Box icon ={"directions_walk"} 
+               color= {"black"} 
+               value = {this.state.steps} 
+               unit = {"Steps"}
+               min={stepsMin}
+               max={stepsMax}
+               onChange={this.onStepsChange.bind(this)}/>
+          <Box icon ={"favorite"} 
+               color= {"red"} 
+               value = {this.state.heart} 
+               unit = {"bpm"}
+               min = {heartMin}
+               max = {heartMax}
+               onChange = {this.onHeartChange.bind(this)}
+               />
+          <Box icon ={"wb_sunny"} 
+               color= {"yellow"} 
+               value = {this.state.temperature} 
+               unit = {"°C"}
+               min={tempMin}
+               max={tempMax}
+               onChange={this.onTemperatureChange.bind(this)}/>
           <p>Heart: {heartMin}</p>
           <p>Temperature : {tempMin}</p>
           <p>Steps : {stepsMin}</p>
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -3,13 +3,32 @@ import React from "react";
 class Box extends React.Component {
 render() {
     const {color, icon, value, unit} = this.props;
+    if(icon === "local_drink"){
+        return(
+            <div className = "box col-sm-3 col-6">
+            <span class="material-icons"
+                  style= {{fontSize: 100, color: color}}>
+                  {icon}
+            </span>
+            
+            <p>{value} {unit}</p>
+           
+        </div>
+        )
+    }
     return(
         <div className = "box col-sm-3 col-6">
             <span class="material-icons"
                   style= {{fontSize: 100, color: color}}>
                   {icon}
             </span>
+            
             <p>{value} {unit}</p>
+            <input type = "range" 
+                   min = {this.props.min} 
+                   max = {this.props.max} 
+                   value = {this.props.value} 
+                   onInput = {this.props.onChange}></input>
         </div>
     )
 }
